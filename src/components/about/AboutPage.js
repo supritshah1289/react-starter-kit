f33f96d38c8ec484e803f2d842807bd635351385
supritkumar shah <suprit7@gmail.com>
2017-08-01 19:57:18 -0400

    foundation of the application

---
+++ src/components/about/AboutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class AboutPage extends React.Component{
+
+  render(){
+    return(
+        <div className="jumbotron">
+          <h1>About</h1>
+          <p>This application uses varities of useful libraries</p>
+        </div>
+      );
+  }
+}
+
+export default  AboutPage;
---
+++ src/components/course/CoursesPage.js
@@ -0,0 +1,14 @@
+import React, {PropType} from 'react';
+
+
+class CoursesPage extends React.Component{
+  render(){
+    return(
+      <div>
+        <h1>Courses</h1>
+      </div>
+    );
+  }
+}
+
+export default CoursesPage;
